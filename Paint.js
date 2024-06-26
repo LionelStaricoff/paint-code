825121a75b596649caff2514d21e977bcb5e12d6
lionel staricoff <staricofflionel@hotmail.com>
2024-06-25 19:09:56 -0500

    refactorizando metodo colorear para que use un objeto regex javascript

--- Paint.js
+++ Paint.js
@@ -1,5 +1,5 @@
 
-export  class ColorearHtml {
+export class ColorearHtml {
     constructor() {
         this.etiquetas = 'yellowgreen';
         this.atributos = 'palevioletred';
@@ -12,74 +12,31 @@ export  class ColorearHtml {
     reemplazar() {
         document.addEventListener('DOMContentLoaded', () => {
             const codigoHtml = document.querySelectorAll('.colorear');
-            codigoHtml.forEach(c => this.colorear(c));
+            codigoHtml.forEach(c => this.colorar(c));
 
         });
     }
 
-    colorear(codigoHtml) {
+
+    colorar(codigoHtml) {
         codigoHtml.style.backgroundColor = "#333333";
         codigoHtml.style.padding = '1em';
         codigoHtml.style.color = 'gainsboro';
-
         const contenido = codigoHtml.innerHTML;
+        const etiquetas = ["header", "main", "footer", "section", "h1", "H1", "h2", "H2", "h3", "H3", "h4", "H4", "h5", "H5", "h6", "H6",
+             "button", "ul", "li", "nav","pre","hr","ol","blockquote"];
 
-        const contenidoModificado = contenido
-            .replace(/class/g, `<L style="color: ${this.atributos};">class</L>`)
-            .replace(/id/g, `<L style="color: ${this.atributos};">id</L>`)
+        let contenidoModificado = contenido
+        etiquetas.forEach(etiqueta => {
 
-            .replace(/<header/g, `<br> &lt;<L style="color: ${this.etiquetas};">header</L>`)
-            .replace(/<\/header>/g, `&lt;<L style="color: ${this.etiquetas};">/header</L>&gt;`)
-            .replace(/<main/g, `<br> &lt;<L style="color: ${this.etiquetas};">main</L>`)
-            .replace(/<\/main>/g, `&lt;<L style="color: ${this.etiquetas};">/main</L>&gt;`)
-            .replace(/<footer/g, `<br> &lt;<L style="color: ${this.etiquetas};">footer</L>`)
-            .replace(/<\/footer>/g, `&lt;<L style="color: ${this.etiquetas};">/footer</L>&gt;`)
-            .replace(/<section/g, `<br> &lt;<L style="color: ${this.etiquetas};">section</L>`)
-            .replace(/<\/section>/g, `&lt;<L style="color: ${this.etiquetas};">/section</L>&gt;`)
-            .replace(/<h1/g, `<br> &lt;<L style="color: ${this.etiquetas};">h1</L>`)
-            .replace(/<\/h1>/g, `&lt;<L style="color: ${this.etiquetas};">/h1</L>&gt;`)
-            .replace(/<H1/g, `<br> &lt;<L style="color: ${this.etiquetas};">H1</L>`)
-            .replace(/<\/H1>/g, `&lt;<L style="color: ${this.etiquetas};">/H1</L>&gt;`)
-            .replace(/<h2/g, `<br> &lt;<L style="color: ${this.etiquetas};">h2</L>`)
-            .replace(/<\/h2>/g, `&lt;<L style="color: ${this.etiquetas};">/h2</L>&gt;`)
-            .replace(/<H2/g, `<br> &lt;<L style="color: ${this.etiquetas};">H2</L>`)
-            .replace(/<\/H2>/g, `&lt;<L style="color: ${this.etiquetas};">/H2</L>&gt;`)
-            .replace(/<H3/g, `<br> &lt;<L style="color: ${this.etiquetas};">H3</L>`)
-            .replace(/<\/H3>/g, `&lt;<L style="color: ${this.etiquetas};">/H3</L>&gt;`)
-            .replace(/<h3/g, `<br> &lt;<L style="color: ${this.etiquetas};">h3</L>`)
-            .replace(/<\/h3>/g, `&lt;<L style="color: ${this.etiquetas};">/h3</L>&gt;`)
-            .replace(/<h4/g, `<br> &lt;<L style="color: ${this.etiquetas};">h4</L>`)
-            .replace(/<\/h4>/g, `&lt;<L style="color: ${this.etiquetas};">/h4</L>&gt;`)
-            .replace(/<H4/g, `<br> &lt;<L style="color: ${this.etiquetas};">H4</L>`)
-            .replace(/<\/H4>/g, `&lt;<L style="color: ${this.etiquetas};">/H4</L>&gt;`)
-            .replace(/<H5/g, `<br>&lt;<L style="color: ${this.etiquetas};">H5</L>`)
-            .replace(/<\/H5>/g, `&lt;<L style="color: ${this.etiquetas};">/H5</L>&gt;`)
-            .replace(/<h5/g, `<br> &lt;<L style="color: ${this.etiquetas};">h5</L>`)
-            .replace(/<\/h5>/g, `&lt;<L style="color: ${this.etiquetas};">/h5</L>&gt;`)
-            .replace(/<h6/g, `<br> &lt;<L style="color: ${this.etiquetas};">h6</L>`)
-            .replace(/<\/h6>/g, `&lt;<L style="color: ${this.etiquetas};">/h6</L>&gt;`)
-            .replace(/<H6/g, `<br> &lt;<L style="color: ${this.etiquetas};">H6</L>`)
-            .replace(/<\/H6>/g, `&lt;<L style="color: ${this.etiquetas};">/H6</L>&gt;`)
-            .replace(/<button/g, `<br> &lt;<L style="color: ${this.etiquetas};">button</L>`)
-            .replace(/<\/button>/g, `&lt;<L style="color: ${this.etiquetas};">/button</L>&gt;`)
-            .replace(/<ul/g, `<br> &lt;<L style="color: ${this.etiquetas};">ul</L>`)
-            .replace(/<\/ul>/g, `&lt;<L style="color: ${this.etiquetas};">/ul</L>&gt;`)
-            .replace(/<li/g, `<br> &lt;<L style="color: ${this.etiquetas};">li</L>`)
-            .replace(/<\/li>/g, `&lt;<L style="color: ${this.etiquetas};">/li</L>&gt;`)
-            .replace(/<nav/g, `<br> &lt;<L style="color: ${this.etiquetas};">nav</L>`)
-            .replace(/<\/nav>/g, `&lt;<L style="color: ${this.etiquetas};">/nav</L>&gt;`)
-            .replace(/<hr/g, `<br> &lt;<L style="color: ${this.etiquetas};">hr</L>`)
-            .replace(/<pre/g, `<br> &lt;<L style="color: ${this.etiquetas};">pre</L>`)
-            .replace(/<\/pre>/g, `&lt;<L style="color: ${this.etiquetas};">/pre</L>&gt;`)
-            .replace(/<blockquote/g, `<br> &lt;<L style="color: ${this.etiquetas};">blockquote</L>`)
-            .replace(/<\/blockquote>/g, `&lt;<L style="color: ${this.etiquetas};">/blockquote</L>&gt;`)
-            .replace(/<ol/g, `<br> &lt;<L style="color: ${this.etiquetas};">ol</L>`)
-            .replace(/<\/ol>/g, `&lt;<L style="color: ${this.etiquetas};">/ol</L>&gt;`)
-           
-            ;
+            contenidoModificado = contenidoModificado
+                .replace(new RegExp(`<${etiqueta}\\b`, 'g'), `<br> &lt;<L style="color: ${this.etiquetas};">${etiqueta}</L>`)
+                .replace(new RegExp(`</${etiqueta}>`, 'g'), `&lt;<L style="color: ${this.etiquetas};">/${etiqueta}</L>&gt;`);
+        });
 
         codigoHtml.innerHTML = contenidoModificado;
     }
+
     espaciar() {
 
         const codigoHtml = document.querySelectorAll('.colorear');
@@ -132,7 +89,7 @@ export  class ColorearHtml {
         div.style.marginTop = '-20px';
         let padre;
         const hijos1 = Array.from(hijos);
-   
+
         for (const ch of hijos1) {
             padre = ch.parentNode;
             div.appendChild(ch);
